b9d56738f03c9ef2caad0961343f1d4048aa9fcb
Hanish <hanish.maram@gmail.com>
2023-12-29 17:28:00 +0000

    * task model * create tasks endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,43 @@
 const dotenv = require("dotenv");
 const express = require('express');
 const connectDB = require("./config/connectDB");
+const Task = require("./model/taskModel");
+
 
 const app = express();
 
 dotenv.config();
 
+app.use(express.json());
+
+// parse the data from url encoded data
+//app.use(express.urlencoded({extended:false}));
+
+function logger(req, res, next) {
+    console.log("Logger");
+    console.log(req.method);
+
+    next();
+    console.log("After request");
+}
+
 // Routes
 app.get("/", (req,res) => {
     res.send("Home Page");
+})
+
+// Create a Task
+app.post("/api/tasks", logger, async (req, res) => {
+
+    try {
+    
+        const task = await Task.create(req.body);
+        res.status(200).json(task);
+
+    } catch (error) {
+        res.status(500).send({msg: error.message});
+    }
+
 })
 
 
