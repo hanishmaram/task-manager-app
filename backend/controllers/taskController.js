bbebdb3bef1ef9ccfef024f97f73001b4a8c4ad4
Hanish <hanish.maram@gmail.com>
2023-12-30 09:43:30 +0000

    * get single task * put request task * delete task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Task = require("../models/taskModel");
 
 // Create a Task
@@ -28,7 +29,70 @@ const getTasks = async (req,res) => {
 
 }
 
+const getTask = async (req, res) => {
+
+    try {
+        
+        const {id} = req.params;
+        const task = await Task.findById(id);
+        if(!task){
+            return res.status(404).json({msg: `Task not found for id : ${id}`});
+        }
+        res.status(200).json(task);
+
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+
+}
+
+// Delete task
+const deleteTask = async (req, res) => {
+
+    try {
+        
+        const {id} = req.params;
+        const task = await Task.findByIdAndDelete(id);
+
+        if(!task) {
+            return res.status(404).json({msg: `No task with id : ${id}`});
+        }
+
+        res.status(200).send("Task deleted.")
+
+    } catch (error) {
+        res.status(500).json({msg:error.message});
+    }
+
+}
+
+// Update a Task
+const updateTask = async (req, res) => {
+
+    try {
+        
+        const {id} = req.params;
+        const {name, completed} = req.body;
+        const task = await Task.findByIdAndUpdate(
+            {_id : id}, req.body, {new: true, runValidators: true} 
+            );
+
+        if(!task) {
+            return res.status(404).json({msg:`Task with given id not available id: ${id}`});
+        }
+        res.status(200).json(task);
+
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+
+}
+
+
 module.exports = { 
     createTask,
-    getTasks
+    getTasks,
+    getTask,
+    deleteTask,
+    updateTask
  }
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const Task = require('../models/taskModel');
-const { createTask, getTasks } = require('../controllers/taskController');
+const { createTask, getTasks, getTask, deleteTask, updateTask } = require('../controllers/taskController');
 const router = express.Router();
 
 
 router.post("/api/tasks",  createTask);
-
-
 router.get("/api/tasks", getTasks);
+router.get("/api/tasks/:id", getTask);
+router.delete("/api/tasks/:id", deleteTask);
+router.put("/api/tasks/:id", updateTask)
 
 
 module.exports = router;
