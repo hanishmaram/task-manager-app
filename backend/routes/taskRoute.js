bb7c6623c8697faac175cf322c55cd4adf7e2d0f
Hanish <hanish.maram@gmail.com>
2023-12-30 07:27:09 +0000

    * task controller

---
+++ backend/routes/taskRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Task = require('../model/taskModel');
+const { createTask, getTasks } = require('../controllers/taskController');
+const router = express.Router();
+
+
+router.post("/api/tasks",  createTask);
+
+
+router.get("/api/tasks", getTasks);
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const dotenv = require("dotenv");
 const express = require('express');
 const connectDB = require("./config/connectDB");
-const Task = require("./model/taskModel");
+const taskRoutes = require('./routes/taskRoute');
+//const Task = require("./model/taskModel");
 
 
 const app = express();
@@ -13,32 +14,22 @@ app.use(express.json());
 // parse the data from url encoded data
 //app.use(express.urlencoded({extended:false}));
 
-function logger(req, res, next) {
-    console.log("Logger");
-    console.log(req.method);
+app.use(taskRoutes);
 
-    next();
-    console.log("After request");
-}
+// function logger(req, res, next) {
+//     console.log("Logger");
+//     console.log(req.method);
+
+//     next();
+//     console.log("After request");
+// }
 
 // Routes
 app.get("/", (req,res) => {
     res.send("Home Page");
 })
 
-// Create a Task
-app.post("/api/tasks", logger, async (req, res) => {
 
-    try {
-    
-        const task = await Task.create(req.body);
-        res.status(200).json(task);
-
-    } catch (error) {
-        res.status(500).send({msg: error.message});
-    }
-
-})
 
 
 const PORT = process.env.PORT || 5000; 
